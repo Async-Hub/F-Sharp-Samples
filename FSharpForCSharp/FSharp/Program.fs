729343640795afc8b78161197247f3ffbf6b8d61
Karen Tazayan <karen.tazayan@outlook.com>
2020-10-15 12:58:46 +0400

    Added "Pattern Matching" sample into "FSharpSyntaxInShort.fsx".

--- FSharpForCSharp/FSharp/Program.fs
+++ FSharpForCSharp/FSharp/Program.fs
@@ -1,8 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open FSharp
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let account = { Id = "1"; CreationTime = DateTime.Now}
+    printfn "Account id: %A %A" account.Id account.CreationTime
     0 // return an integer exit code
--- FSharpForCSharp/REPL/FSharpSyntaxInShort.fsx
+++ FSharpForCSharp/REPL/FSharpSyntaxInShort.fsx
@@ -1,6 +1,5 @@
 // ******* Declarations *******
 
-
 let sampleInt = 7
 let sampleFloat = 7.27f
 let sampleDouble = 7.27
@@ -11,12 +10,10 @@ mutableInt <- 8
 
 let sampleTuple = 5,9
 
-
 // ******* Functions *******
 
-
-let addTwo a: int = a + 2
-let addThree a = a + 3 // Type inference
+let addTwo a = a + 2
+let addThree a = a + 3.0 // Type inference
 let sumTuple (a, b) = a + b // Tuple parameter
 
 let squareInt x = x * x 
@@ -25,12 +22,10 @@ let ``Sum Of Squares`` = List.map squareInt [1..10] |> List.sum
 // ******* Collection Types *******
 // https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/fsharp-collection-types
 
-
 let sampleList = ["a";"b";"c";"d";"e"]
 let sampleArray = [|"a";"b";"c";"d";"e"|]
 
 // ******* Complex Types *******
-
 
 type Customer = { First: string; Last: string; } // Record type
 
@@ -44,6 +39,24 @@ let point = { X = 1.0; Y = 2.0 }
 
 // ******* Object-Oriented constructs *******
 
-
+//[<AllowNullLiteral>]
+type Account(id : string, creationTime : System.DateTime) =
+    member  _.Id = id
+    member _.CreationTime = creationTime
+    
+let mutable account = Account("1", System.DateTime.Now)
+//account <- null
 
 // ******* Pattern Matching *******
+
+type Shape =
+    | Rectangle of height : float * width : float
+    | Circle of radius : float
+    
+let matchShape shape =
+    match shape with
+    | Rectangle(height = h) -> printfn "Rectangle with length %f" h
+    | Circle(r) -> printfn "Circle with radius %f" r
+    
+let shape = Rectangle(3.0, 4.5)
+matchShape shape
